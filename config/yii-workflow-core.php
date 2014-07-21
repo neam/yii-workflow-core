95bd91d32aa32f5462d4bcfc886ce886d276bd5d
Fredrik Wollsén <fredrik@neam.se>
2014-07-21 18:26:50 +0300

    Necessary refactoring

--- config/yii-workflow-core.php
+++ config/yii-workflow-core.php
@@ -1,7 +1,5 @@
 <?php
 
-$config['import'][] = 'vendor.neam.yii-workflow-core.traits.ItemControllerTrait';
 $config['import'][] = 'vendor.neam.yii-workflow-core.traits.ItemTrait';
-$config['import'][] = 'vendor.neam.yii-workflow-core.helpers.ItemControllerConstants';
 $config['import'][] = 'vendor.neam.yii-app-metadata.Labels';
 $config['import'][] = 'vendor.neam.yii-restricted-access.DataModel';
